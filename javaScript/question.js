257d3087ab8d79e50fb0a287e180f9ba205d17af
aniket-6234 <aniketashok1999@gmail.com>
2022-04-21 08:29:04 +0530

    quiz

---
+++ javaScript/question.js
@@ -0,0 +1,65 @@
+//creating an array and passing the number, questions, options, and answer.
+let questions = [
+  {
+    numb: 1,
+    question: "Who is the first Indian to recieve Nobel Prize?",
+    answer: "Ravinder Nath Tagore",
+    options: [
+      "Mark Zukerburg",
+      "Elon Musk",
+      "Ravinder Nath Tagore",
+      "Jeff Bezos",
+    ]
+  },
+
+  {
+    numb: 2,
+    question: "Teacher Day celebrated?",
+    answer: "5 September",
+    options: [
+      "5 September",
+      "12 January",
+      "8 October",
+      "15 July",
+    ]
+  },
+
+  {
+    numb: 3,
+    question: "When was the nuclear bomb was dropped in Japan?",
+    answer: "1945",
+    options: [
+      "1928",
+      "1945",
+      "1954",
+      "1967",
+    ]
+  },
+
+  {
+    numb: 4,
+    question: "Who is knowing as 'Flying Sikh'?",
+    answer: "Milkha Singh",
+    options: [
+      "Harbajan Singh",
+      "Ketan Singh",
+      "Milkha Singh",
+      "Honey Singh",
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "National Defence Academy is located in?",
+    answer: "Dehradun",
+    options: [
+      "Kolkatta",
+      "Shimla",
+      "Lucknow",
+      "Dehradun",
+    ]
+  },
+
+
+
+];
